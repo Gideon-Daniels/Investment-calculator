aab3e775e5a9b1483ada07598a969b95b87b77b8
Gideon Daniels <gideon.daniels@safetyio.com>
2024-10-03 22:59:45 +0200

    added investment-table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { InvestmentFormComponent } from './investment-form/investment-form.component';
+import { InvestmentTableComponent } from './investment-table/investment-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, InvestmentFormComponent],
+  imports: [HeaderComponent, InvestmentFormComponent, InvestmentTableComponent],
 })
 export class AppComponent {}
---
+++ src/app/investment-table/investment-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-table.component.html',
+  styleUrl: './investment-table.component.css'
+})
+export class InvestmentTableComponent {
+
+}

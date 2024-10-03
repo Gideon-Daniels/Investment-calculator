d4ee8b19b76ef52fb6eed60195c33f436aac9c51
Gideon Daniels <gideon.daniels@safetyio.com>
2024-10-03 22:43:28 +0200

    added investment-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { InvestmentFormComponent } from './investment-form/investment-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InvestmentFormComponent],
 })
 export class AppComponent {}
---
+++ src/app/investment-form/investment-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-form.component.html',
+  styleUrl: './investment-form.component.css'
+})
+export class InvestmentFormComponent {
+
+}
